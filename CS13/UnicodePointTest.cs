935653cb127171b1fa2fd34fde9acf0d9d9b276d
Admin <admin@mutuware.com>
2024-11-18 20:35:27 +0000

    Add unicode test

---
+++ CS13/UnicodePointTest.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace CS13
+{
+    public class UnicodePointTest
+    {
+        [Fact]
+        public void NewlineUnicode()
+        {
+            var c = '\n';
+            var str = $"U+{(int)c:x4}";
+            Assert.Equal("U+000a", str);
+        }
+
+        [Fact]
+        public void TabUnicode()
+        {
+            var c = '\t';
+            var str = $"U+{(int)c:x4}";
+            Assert.Equal("U+0009", str);
+        }
+
+        [Fact]
+        public void EscapeUnicode()
+        {
+            var c = '\e'; // new in C#13
+            var str = $"U+{(int)c:x4}";
+            Assert.Equal("U+001b", str);
+        }
+
+    }
+}

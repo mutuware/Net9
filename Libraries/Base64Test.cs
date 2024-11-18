7ae194ee2aadfa2e3dcdbe59427cdb77f8572e19
Admin <admin@mutuware.com>
2024-11-18 21:11:49 +0000

    Add Base64 test

---
+++ Libraries/Base64Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Buffers.Text;
+using System.Text;
+using Xunit;
+
+namespace Libraries
+{
+    public class Base64Test
+    {
+        [Fact]
+        public void Encode()
+        {
+            ReadOnlySpan<byte> bytes = "abc"u8;
+            var encoded = Base64Url.EncodeToString(bytes);
+            Assert.Equal("YWJj", encoded);
+        }
+
+        [Fact]
+        public void Decode()
+        {
+            ReadOnlySpan<byte> bytes = "YWJj"u8;
+            var decoded = Base64Url.DecodeFromUtf8(bytes);
+            var result = Encoding.UTF8.GetString(decoded, 0, decoded.Length);
+            Assert.Equal("abc", result);
+        }
+    }
+}

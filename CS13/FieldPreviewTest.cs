52c8e2e58b31ed1f2939103ea46a9fb97c9d4cb4
Admin <admin@mutuware.com>
2024-11-18 20:35:18 +0000

    Add field keyword (preview)

---
+++ CS13/FieldPreviewTest.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace CS13
+{
+    public class FieldPreviewTest
+    {
+        [Fact]
+        public void WidgetCount_Assigned()
+        {
+            var sut = new FieldPreview();
+            sut.WidgetCount = 15;
+            Assert.Equal(15, sut.WidgetCount);
+        }
+
+        [Fact]
+        public void WidgetCount_BelowMin()
+        {
+            var sut = new FieldPreview();
+            sut.WidgetCount = 5;
+            Assert.Equal(10, sut.WidgetCount);
+        }
+    }
+
+    public class FieldPreview
+    {
+        private const int minCount = 10;
+
+        public int WidgetCount
+        {
+            get;
+            set => field = (value >= minCount) ? value : minCount;
+        }
+    }
+}

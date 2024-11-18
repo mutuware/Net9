2db3a0ddde54e005fcfdb543bdb440dd68f1d222
Admin <admin@mutuware.com>
2024-11-18 19:14:58 +0000

    Add project files.

---
+++ CS13/Params.cs
@@ -0,0 +1,43 @@
+﻿namespace CS13
+{
+    public class Params
+    {
+        public string Join(params List<char> chars)
+        {
+            var ret = string.Empty;
+
+            foreach (char c in chars)
+            {
+                ret += $"{c}-";
+            }
+
+            return ret;
+        }
+
+        public string JoinFirst(char first, params List<char> chars)
+        {
+            var ret = first.ToString();
+
+            foreach (char c in chars)
+            {
+                ret += $"{c}-";
+            }
+
+            return ret;
+        }
+
+        public List<T> OmitLast<T>(params List<T> list)
+        {
+            var ret = new List<T>();
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                T t = list[i];
+                ret.Add(t);
+            }
+
+            return ret;
+
+        }
+        //public void CantUseDictionary<TKey, TValue>(params Dictionary<TKey, TValue> dict) { }
+    }
+}
---
+++ CS13/ParamsTest.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace CS13
+{
+    public class ParamsTest
+    {
+        [Fact]
+        public void ParamsCollectionArgument()
+        {
+            var sut = new Params();
+            var actual = sut.Join('a', 'b', 'c');
+            Assert.Equal("a-b-c-", actual);
+        }
+
+        [Fact]
+        public void ParamsCollectionArgumentMultiple()
+        {
+            var sut = new Params();
+            var actual = sut.JoinFirst('a', 'b', 'c', 'd');
+            Assert.Equal("ab-c-d-", actual);
+        }
+
+        [Fact]
+        public void ParamsCollectionGeneric()
+        {
+            var sut = new Params();
+            var actual = sut.OmitLast(1, 2, 3);
+            Assert.Equal(2, actual.Count);
+            Assert.Equal(1, actual[0]);
+            Assert.Equal(2, actual[1]);
+        }
+    }
+}

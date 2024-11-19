8a19d8eac25c7e59022d16eb091766d3c7657ca0
Admin <admin@mutuware.com>
2024-11-19 13:15:41 +0000

    Add timespan test

---
+++ Libraries/TimeSpan.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace Libraries
+{
+    public class TimeSpanTest
+    {
+        [Fact]
+        public void FromSecondsInt()
+        {
+            var ts = TimeSpan.FromSeconds(1, 2, 3); // new in .Net 9
+            Assert.Equal(1, ts.Seconds);
+            Assert.Equal(2, ts.Milliseconds);
+            Assert.Equal(3, ts.Microseconds);
+        }
+    }
+}
